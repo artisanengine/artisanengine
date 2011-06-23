c42231e1f7ec8b58c21a7d1e0e09961152f4d1cb
Reade Harris <services@artisanengine.com>
2011-06-23 13:37:53 -0400

    Add Artisan: Create a Page spec.

---
+++ spec/acceptance/artisan/pages/create_a_page_spec.rb
@@ -0,0 +1,30 @@
+require 'acceptance/acceptance_helper'
+
+feature 'Create a Page', %q{
+  In order to present basic content to my patrons
+  As an artisan
+  I want to create a page.
+} do
+  
+  scenario "An artisan can create a page with valid attributes" do
+    # When I create a page with valid attributes,
+    create_page
+    
+    # Then I should see a notice,
+    page.should have_selector '.notice'
+    
+    # And I should see my page.
+    page.should have_content 'About Me'
+  end
+  
+  scenario "An artisan cannot create a page with invalid attributes" do
+    # When I create a page with invalid attributes,
+    create_page title: ''
+    
+    # Then I should see an error,
+    page.should have_selector '.error'
+    
+    # And there should be no pages.
+    Page.count.should == 0
+  end
+end
