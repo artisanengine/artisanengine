8779f46e252c5fd8973a816eccefceaff6c29177
Reade Harris <services@artisanengine.com>
2011-06-23 12:32:56 -0400

    Add Create a Frame feature.

---
+++ spec/acceptance/engineer/frames/create_a_frame_spec.rb
@@ -0,0 +1,37 @@
+require 'acceptance/acceptance_helper'
+
+feature 'Create a Frame', %q{
+  In order to set up a new artisan's home on ArtisanEngine
+  As an engineer
+  I want to create a frame.
+} do
+  
+  background do
+    # Given I am on the New Frame page,
+    visit new_frame_page
+  end
+  
+  scenario "An engineer can create a frame with valid attributes" do
+    # When I create a frame with valid attributes,
+    create_frame name: 'Haus Leather'
+    
+    # Then I should see a notice,
+    page.should have_selector '.notice'
+    
+    # And I should see my new frame.
+    page.should have_content 'Haus Leather'
+  end
+  
+  scenario "An engineer cannot create a frame with invalid attributes" do
+    pending
+    
+    # When I create a frame with invalid attributes,
+    create_frame name: 'Haus Leather', domain: ''
+    
+    # Then I should see an error,
+    page.should have_selector '.error'
+    
+    # And there should be no new frames.
+    Frame.count.should == 1
+  end
+end
