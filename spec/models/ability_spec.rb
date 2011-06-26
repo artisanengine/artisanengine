46aff06ed88003ed706ed060c7189b5cd448403c
Reade Harris <services@artisanengine.com>
2011-06-26 17:28:48 -0400

    Clean up ability spec.

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -1,40 +1,42 @@
 require 'spec_helper'
 require 'cancan/matchers'
 
-describe "Engineer Abilities:" do
-  let( :user )    { Factory :user, role: 'Engineer' }
-  let( :frame )   { Factory :frame }
-  let( :ability ) { Ability.new( user, frame ) }
-  
-  it "can perform all actions on all things" do
-    ability.should be_able_to :manage, User
-    ability.should be_able_to :manage, Frame
-    ability.should be_able_to :manage, Object
-  end
-end
+describe "Abilities:" do
+  let( :my_frame )      { Factory :frame }
+  let( :other_frame )   { Factory :frame }
+  let( :current_frame ) { Factory :frame }
+  let( :ability )       { Ability.new( user, current_frame ) }
 
-describe "Artisan Abilities:" do
-  let( :user )    { Factory :user, role: 'Artisan' }
-  let( :frame )   { Factory :frame }
-  let( :ability ) { Ability.new( user, frame ) }
+  context "Engineer Abilities:" do
+    let( :user ) { Factory :user, role: 'Engineer', frame: my_frame }
   
-  it "can create pages in their own frame only" do
-    ability.should be_able_to :create, Page
+    it "can perform all actions on all things" do
+      ability.should be_able_to :manage, User
+      ability.should be_able_to :manage, Frame
+      ability.should be_able_to :manage, Object
+    end
   end
-  
-  it "can read pages in their own frame only" do
-    ability.should_not be_able_to :read, Factory( :page )
-    ability.should be_able_to :read, Factory( :page, frame: user.frame )
-  end
-end
 
-describe "General Abilities:" do
-  let( :user )    { Factory :user }
-  let( :frame )   { Factory :frame }
-  let( :ability ) { Ability.new( user, frame ) }
+  context "Artisan Abilities:" do
+    let( :user ) { Factory :user, role: 'Artisan', frame: my_frame }
   
-  it "can read pages in the current frame" do
-    ability.should_not be_able_to :read, Factory( :page )
-    ability.should be_able_to :read, Factory( :page, frame: frame )
+    it "can only create pages in their own frame" do
+      ability.should be_able_to :create,     Factory.build( :page, frame: my_frame )
+      ability.should_not be_able_to :create, Factory.build( :page, frame: other_frame )
+    end
+  
+    it "can only read pages in their own frame" do
+      ability.should_not be_able_to :read, Factory( :page, frame: other_frame )
+      ability.should be_able_to :read,     Factory( :page, frame: my_frame )
+    end
+  end
+
+  context "Visitor Abilities:" do
+    let( :user ) { Factory :user }
+  
+    it "can read pages in the current frame" do
+      ability.should_not be_able_to :read, Factory( :page, frame: other_frame )
+      ability.should be_able_to :read,     Factory( :page, frame: current_frame )
+    end
   end
 end
