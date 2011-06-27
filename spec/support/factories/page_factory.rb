face95ca55b39a71bfb7b5a35e7a3cd9f8f33089
Reade Harris <services@artisanengine.com>
2011-06-26 20:18:43 -0400

    Faker returning arrays of Lorem was breaking shit.

--- spec/support/factories/page_factory.rb
+++ spec/support/factories/page_factory.rb
@@ -2,5 +2,5 @@ Factory.define :page do |f|
   f.frame       { Frame.find_or_create_by_domain( 'example.com', name: 'Example Frame' ) }
   
   f.title       { Faker::Company.catch_phrase }
-  f.content     { Faker::Lorem.paragraphs( 3 ) }
+  f.content     { Faker::Lorem.paragraphs( 3 ).join( ' ' ) }
 end
