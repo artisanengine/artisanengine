ec275466e1399750c9f71483d42bd9aecfcd5227
Reade Harris <services@artisanengine.com>
2011-06-27 23:05:43 -0400

    I was wondering why that looked like crap.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,6 @@ class ApplicationController < ActionController::Base
   
   # Render the static 404: Not Found page.
   def render_404
-    render 'public/404.html', status: 404
+    render 'public/404.html', layout: false, status: 404
   end
 end
