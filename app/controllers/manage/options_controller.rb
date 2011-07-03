ff36ba1ac997c974497b82a753bc92a5fcf699f0
Reade Harris <services@artisanengine.com>
2011-07-03 18:02:33 -0400

    Update an option

--- app/controllers/manage/options_controller.rb
+++ app/controllers/manage/options_controller.rb
@@ -9,6 +9,23 @@ module Manage
         redirect_to( edit_manage_good_path( @good ), alert: "Option could not be added." )
     end
     
+    def edit
+      @good   = current_frame.goods.find( params[ :good_id ] )
+      @option = @good.options.find( params[ :id ] )
+    end
+    
+    def update
+      @good   = current_frame.goods.find( params[ :good_id ] )
+      @option = @good.options.find( params[ :id ] )
+      
+      if @option.update_attributes( params[ :option ] )
+        redirect_to edit_manage_good_path( @good ), notice: "Option was successfully updated."
+      else
+        flash[ :alert ] = t( :form_alert )
+        render :edit
+      end
+    end
+    
     def destroy
       @good   = current_frame.goods.find( params[ :good_id ] )
       @option = @good.options.find( params[ :id ] )
---
+++ spec/acceptance/artisans/goods/options/update_an_option_spec.rb
@@ -0,0 +1,56 @@
+require 'acceptance/acceptance_helper'
+
+feature 'Update an Option', %q{
+  In order to change the display of variations of my good
+  As an artisan
+  I want to update an option.
+} do
+  
+  background do
+    # Given I am signed in as an artisan,
+    sign_in_as_artisan
+    
+    # And I have created a good,
+    @good = Good.generate name: 'Freeze Ray'
+    
+    # And my good has two additional options,
+    @good.options << Option.spawn( name: 'Destructiveness', default_value: 'Low' )
+    @good.options << Option.spawn( name: 'Horribleness',    default_value: 'High' )
+    
+    # And I am on the edit good page for the good,
+    visit edit_good_page_for 'Freeze Ray'
+    
+    # And I have clicked the Edit link for Destructiveness,
+    within '.option', text: 'Destructiveness' do
+      click_link 'Edit'
+    end
+  end
+  
+  scenario "An artisan can update an option with valid attributes" do
+    # When I fill in valid information for the updated option,
+    fill_in 'Name', with: 'Destructive Capacity'
+    
+    # And I click Update Option,
+    click_button 'Update Option'
+    
+    # Then I should see a notice,
+    page_should_have_notice
+    
+    # And I should see my new option.
+    page.should have_content 'Destructive Capacity'
+  end
+  
+  scenario "An artisan cannot update an option with invalid attributes" do
+    # When I fill in invalid information for the updated option,
+    fill_in 'Name', with: ''
+    
+    # And I click Update Option,
+    click_button 'Update Option'
+    
+    # Then I should see an alert,
+    page_should_have_alert
+    
+    # And my option's attributes should not change.
+    @good.options[1].name.should == 'Destructiveness'
+  end
+end
