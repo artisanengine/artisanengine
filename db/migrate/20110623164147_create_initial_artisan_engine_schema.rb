091f8ff1f417d18046bb6dacc14c0a07367e13f5
Reade Harris <services@artisanengine.com>
2011-07-05 20:31:24 -0400

    anal

--- db/migrate/20110623164147_create_initial_artisan_engine_schema.rb
+++ db/migrate/20110623164147_create_initial_artisan_engine_schema.rb
@@ -190,7 +190,7 @@ class CreateInitialArtisanEngineSchema < ActiveRecord::Migration
     end
     
     add_index :slugs, :sluggable_id
-    add_index :slugs, [:name, :sluggable_type, :sequence, :scope], :name => "index_slugs_on_n_s_s_and_s", :unique => true
+    add_index :slugs, [ :name, :sluggable_type, :sequence, :scope ], name: "index_slugs_on_n_s_s_and_s", unique: true
     
   end
 
