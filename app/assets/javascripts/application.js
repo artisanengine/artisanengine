7073a3b0b3a1eb3427fbe2696fc7b237af2b2534
Reade Harris <services@artisanengine.com>
2011-07-09 15:15:05 -0400

    Install Underscore.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,7 @@
 //= require jquery
 //= require jquery-ui
 //= require jquery_ujs
+//= require underscore
 //= require colorbox/jquery.colorbox-min
 //= require tokeninput/jquery.tokeninput
 //= require_tree .
