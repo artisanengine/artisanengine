7800650bc716317f514cfa8ab0e85ece4d06bdaf
Reade Harris <services@artisanengine.com>
2011-07-09 18:51:59 -0400

    Basic styling for display case pages

--- app/controllers/visit/display_cases_controller.rb
+++ app/controllers/visit/display_cases_controller.rb
@@ -1,7 +1,15 @@
 module Visit
   class DisplayCasesController < Visit::VisitController
+    layout :display_cases_or_visit
+    
     def show
       @case = current_frame.display_cases.find( params[ :id ] )
+    end
+    
+    private
+    
+    def display_cases_or_visit
+      template_exists?( "layouts/display_cases" ) ? 'display_cases' : 'visit'
     end
   end
 end
--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,6 +1,20 @@
 module ImagesHelper
   def display_image( image, size = '200x200#' )
-    return nil unless image.image
-    image_tag( image.image.thumb( size ).url )
+    unless image.nil?
+      image_tag( image.image.thumb( size ).url )
+    else
+      placeholder_image( size )
+    end
+  end
+  
+  def placeholder_image( size = '200x200#', options = {} )
+    width  = size.split( 'x' )[0]
+    height = size.split( 'x' )[1].gsub( '#', '' )
+    
+    options[ :alt ]   ||= 'Placeholder Image'
+    options[ :title ] ||= 'Placeholder Image'
+    options[ :style ] ||= "width: #{ width }px; height: #{ height }px;"
+    
+    image_tag 'placeholder.jpg', options
   end
 end
