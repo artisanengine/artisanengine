a9a2242f2fed428ad6dca6d3253970d404b20b14
Reade Harris <services@artisanengine.com>
2011-07-09 19:04:15 -0400

    Featured Products cell

---
+++ app/cells/featured_products_cell.rb
@@ -0,0 +1,13 @@
+class FeaturedProductsCell < Cell::Rails
+  include FramesHelper
+  include ArtisanEngine::Theming
+
+  def display( state )
+    frame = state[ :frame ]
+    limit = state[ :limit ] || 1
+    
+    @goods = frame.display_cases.find_by_name( 'Featured' ).goods.limit( limit )
+    render
+  end
+
+end
---
+++ spec/cells/featured_products_cell_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe FeaturedProductsCell do
+  context "display: " do
+    let( :frame )        { Frame.generate domain: 'test.host' }
+    let( :display_case ) { frame.display_cases.first } # Auto-generated on frame creation.
+    
+    let( :cell )  { render_cell( :featured_products, :display, frame: frame ) }
+    
+    before do
+      # Given there are three Featured goods in the featured case,
+      display_case.goods << Good.spawn( name: 'Product 1' )
+      display_case.goods << Good.spawn( name: 'Product 2' )
+      display_case.goods << Good.spawn( name: 'Product 3' )
+    end
+    
+    it "shows the first featured good in the case by default" do      
+      cell.should have_content     'Product 1'
+      cell.should_not have_content 'Product 2'
+      cell.should_not have_content 'Product 3'
+    end
+    
+    it "shows as many goods as the limit argument requests" do
+      cell = render_cell( :featured_products, :display, frame: frame, limit: 3 )
+      
+      cell.should have_content 'Product 1'
+      cell.should have_content 'Product 2'
+      cell.should have_content 'Product 3'
+    end
+  end
+end
