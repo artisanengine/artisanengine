c461b922ef4129ca77466d295da68293abb25194
Reade Harris <services@artisanengine.com>
2011-07-10 19:15:16 -0400

    Refactor PostsController

--- app/controllers/manage/posts_controller.rb
+++ app/controllers/manage/posts_controller.rb
@@ -2,36 +2,24 @@ module Manage
   class PostsController < Manage::ManageController
     respond_to :html
     
-    def new
-      @blog = current_frame.blog
-      @post = @blog.posts.new
-    end
-    
+    expose( :blog )  { current_frame.blog }
+    expose( :posts ) { blog.posts }
+    expose( :post )
+
     def create
-      @blog = current_frame.blog
-      @post = @blog.posts.build( params[ :post ] )
-      
-      @post.save ?
-        flash[ :notice ] = "Post: #{ @post.title } was successfully created." :
+      post.save ?
+        flash[ :notice ] = "Post: #{ post.title } was successfully created." :
         flash[ :alert ]  = t( :form_alert )
       
-      respond_with :manage, :blog, @post, location: manage_blog_path
+      respond_with :manage, :blog, post, location: manage_blog_path
     end
-        
-    def edit
-      @blog = current_frame.blog
-      @post = @blog.posts.find( params[ :id ] )
-    end
-    
+
     def update
-      @blog = current_frame.blog
-      @post = @blog.posts.find( params[ :id ] )
-      
-      @post.update_attributes( params[ :post ] ) ?
-        flash[ :notice ] = "Post: #{ @post.title } was successfully updated." :
+      post.update_attributes( params[ :post ] ) ?
+        flash[ :notice ] = "Post: #{ post.title } was successfully updated." :
         flash[ :alert ]  = t( :form_alert )
       
-      respond_with :manage, :blog, @post, location: manage_blog_path
+      respond_with :manage, :blog, post, location: manage_blog_path
     end
   end
 end
