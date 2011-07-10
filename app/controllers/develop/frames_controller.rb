9673dbfa62fc8fe23e8b7e7c8dfca33a6e07ee53
Reade Harris <services@artisanengine.com>
2011-07-10 16:59:04 -0400

    Refactor FramesController.

--- app/controllers/develop/frames_controller.rb
+++ app/controllers/develop/frames_controller.rb
@@ -1,23 +1,16 @@
 module Develop
   class FramesController < Develop::DevelopController
     respond_to :html
-  
-    def index
-      @frames = Frame.all
-    end
-  
-    def new
-      @frame = Frame.new
-    end
-
-    def create
-      @frame = Frame.new( params[ :frame ] )
     
-      @frame.save ?
-        flash[ :notice ] = "Frame: #{ @frame.name } was successfully created." :
+    expose( :frames ) { Frame.all }
+    expose( :frame )
+  
+    def create
+      frame.save ?
+        flash[ :notice ] = "Frame: #{ frame.name } was successfully created." :
         flash[ :alert ]  = t( :form_alert )
     
-      respond_with :develop, @frame, location: develop_frames_path
+      respond_with :develop, frame, location: develop_frames_path
     end
   end
 end
