e106625eac8818991a85eac804a0368a08167818
Reade Harris <services@artisanengine.com>
2011-07-10 19:09:59 -0400

    Refactor OptionsController

--- app/controllers/manage/options_controller.rb
+++ app/controllers/manage/options_controller.rb
@@ -1,25 +1,19 @@
 module Manage
   class OptionsController < Manage::ManageController
-    def create
-      @good   = current_frame.goods.find( params[ :good_id ] )
-      @option = @good.options.new( params[ :option ] )
-      
-      @option.save ? 
-        redirect_to( edit_manage_good_path( @good ), notice: "Option was successfully added." ) :
-        redirect_to( edit_manage_good_path( @good ), alert: "Option could not be added." )
-    end
+    expose( :goods )   { current_frame.goods }
+    expose( :good )
+    expose( :options ) { good.options }
+    expose( :option )
     
-    def edit
-      @good   = current_frame.goods.find( params[ :good_id ] )
-      @option = @good.options.find( params[ :id ] )
+    def create
+      option.save ? 
+        redirect_to( edit_manage_good_path( good ), notice: "Option was successfully added." ) :
+        redirect_to( edit_manage_good_path( good ), alert: "Option could not be added." )
     end
     
     def update
-      @good   = current_frame.goods.find( params[ :good_id ] )
-      @option = @good.options.find( params[ :id ] )
-      
-      if @option.update_attributes( params[ :option ] )
-        redirect_to edit_manage_good_path( @good ), notice: "Option was successfully updated."
+      if option.update_attributes( params[ :option ] )
+        redirect_to edit_manage_good_path( good ), notice: "Option was successfully updated."
       else
         flash[ :alert ] = t( :form_alert )
         render :edit
@@ -27,12 +21,9 @@ module Manage
     end
     
     def destroy
-      @good   = current_frame.goods.find( params[ :good_id ] )
-      @option = @good.options.find( params[ :id ] )
-      
-      @option.destroy ?
-        redirect_to( edit_manage_good_path( @good ), notice: "Option was successfully removed." ) :
-        redirect_to( edit_manage_good_path( @good ), alert: "Option could not be removed." )
+      option.destroy ?
+        redirect_to( edit_manage_good_path( good ), notice: "Option was successfully removed." ) :
+        redirect_to( edit_manage_good_path( good ), alert: "Option could not be removed." )
     end
   end
 end
