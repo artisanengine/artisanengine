1f1b2de40b2dbbf95af114874a40b7b800f516a6
Reade Harris <services@artisanengine.com>
2011-07-10 19:18:31 -0400

    Refactor

--- app/controllers/manage/tags_controller.rb
+++ app/controllers/manage/tags_controller.rb
@@ -1,10 +1,10 @@
 module Manage
   class TagsController < Manage::ManageController
+    expose( :tags ) { current_frame.tags }
+    
     def index
-      @tags = current_frame.tags
-      
       respond_to do |format|
-        format.json { render json: @tags.where( "name LIKE ?", "%#{ params[ :q ] }%" ) }
+        format.json { render json: tags.where( "name LIKE ?", "%#{ params[ :q ] }%" ) }
       end
     end
   end
--- app/controllers/manage/variants_controller.rb
+++ app/controllers/manage/variants_controller.rb
@@ -1,25 +1,20 @@
 module Manage
   class VariantsController < Manage::ManageController
-    def create
-      @good    = current_frame.goods.find( params[ :good_id ] )
-      @variant = @good.variants.new( params[ :variant ] )
-      
-      @variant.save ?
-        redirect_to( edit_manage_good_path( @good ), notice: "Variant was successfully created." ) :
-        redirect_to( edit_manage_good_path( @good ), alert: "#{ @variant.errors.full_messages }" )
-    end
+    expose( :goods )        { current_frame.goods }
+    expose( :good )
+    expose( :good_options ) { good.options.in_order }
+    expose( :variants )     { good.variants }
+    expose( :variant )
     
-    def edit
-      @good    = current_frame.goods.find( params[ :good_id ] )
-      @variant = @good.variants.find( params[ :id ] )
+    def create
+      variant.save ?
+        redirect_to( edit_manage_good_path( good ), notice: "Variant was successfully created." ) :
+        redirect_to( edit_manage_good_path( good ), alert: "#{ variant.errors.full_messages }" )
     end
     
     def update
-      @good    = current_frame.goods.find( params[ :good_id ] )
-      @variant = @good.variants.find( params[ :id ] )
-      
-      if @variant.update_attributes( params[ :variant ] )
-        redirect_to edit_manage_good_path( @good ), notice: "Variant was successfully updated."
+      if variant.update_attributes( params[ :variant ] )
+        redirect_to edit_manage_good_path( good ), notice: "Variant was successfully updated."
       else
         flash[ :alert ] = t( :form_alert ) 
         render :edit
@@ -27,12 +22,9 @@ module Manage
     end
     
     def destroy
-      @good    = current_frame.goods.find( params[ :good_id ] )
-      @variant = @good.variants.find( params[ :id ] )
-      
-      @variant.destroy ?
-        redirect_to( edit_manage_good_path( @good ), notice: "Variant was successfully destroyed." ) :
-        redirect_to( edit_manage_good_path( @good ), alert: "#{ @variant.errors.full_messages }" )
+      variant.destroy ?
+        redirect_to( edit_manage_good_path( good ), notice: "Variant was successfully destroyed." ) :
+        redirect_to( edit_manage_good_path( good ), alert: "#{ variant.errors.full_messages }" )
     end
   end
 end
