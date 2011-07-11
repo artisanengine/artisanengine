eab7545eb237473f4d2c5afd29c9c3a7dfdf37ea
Reade Harris <services@artisanengine.com>
2011-07-11 13:24:50 -0400

    Small style tweaks

--- app/cells/recent_posts_cell.rb
+++ app/cells/recent_posts_cell.rb
@@ -4,6 +4,7 @@ class RecentPostsCell < Cell::Rails
 
   def display( state )
     frame  = state[ :frame ]
+    
     @posts = frame.blog.posts.order( "posts.created_at DESC" ).limit( 2 )
     render
   end
