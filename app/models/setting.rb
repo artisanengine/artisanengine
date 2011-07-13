6ffc2487fa4edf0ce8d44217310a4924377807ac
Reade Harris <services@artisanengine.com>
2011-07-13 12:59:29 -0400

    #get_or_set

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -8,4 +8,18 @@ class Setting < ActiveRecord::Base
   # Validations
   
   validates_presence_of :name, :value, :frame
+  
+  # ------------------------------------------------------------------
+  # Methods
+  
+  def self.get_or_set( frame, setting_name, default = nil )
+    setting = self.find_by_frame_id_and_name( frame.id, setting_name )
+    
+    if setting
+      environment_override = ENV[ setting_name.gsub( ' ', '_' ).upcase ]
+      environment_override ? environment_override : setting.value 
+    else
+      default
+    end
+  end
 end
--- spec/models/setting_spec.rb
+++ spec/models/setting_spec.rb
@@ -23,4 +23,30 @@ describe Setting do
       new_setting.should_not be_valid
     end
   end
+  
+  context "methods: " do
+    describe "#get_or_set" do
+      let( :setting ) { Setting.generate name: 'Test Setting', value: 'Test Value' }
+      let( :frame )   { setting.frame }
+      
+      it "finds the correct setting based on the frame and setting name" do
+        Setting.get_or_set( frame, 'Test Setting' ).should == 'Test Value'
+      end
+      
+      context "if no setting is found" do
+        it "returns the default setting" do
+          Setting.get_or_set( frame, 'Nonexistent Setting', 'Default' )
+        end
+      end
+      
+      context "if an environment override is set" do
+        before { ENV[ "TEST_SETTING" ] = "Environment Value" }
+        
+        it "returns the environment setting" do
+          Setting.get_or_set( frame, 'Test Setting', 'Default' )
+                 .should == "Environment Value"
+        end
+      end
+    end
+  end
 end
