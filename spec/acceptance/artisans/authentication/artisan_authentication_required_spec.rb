021908341b0a6cef2c89bb52cf0e372af5e85acc
Reade Harris <services@artisanengine.com>
2011-07-22 21:15:25 -0400

    Engineers bypass artisan authentication

--- spec/acceptance/artisans/authentication/artisan_authentication_required_spec.rb
+++ spec/acceptance/artisans/authentication/artisan_authentication_required_spec.rb
@@ -16,4 +16,18 @@ feature 'Artisan Authentication Required for Artisan-Only Functions', %q{
     # Then I should be prompted to sign in.
     page.current_path.should == artisan_sign_in_page
   end
+  
+  scenario "An engineer can bypass artisan authentication" do
+    # Given I am browsing the default frame,
+    browse_frame
+    
+    # And I am signed in as an engineer,
+    sign_in_as_engineer
+    
+    # When I attempt to access an artisan-only function,
+    visit new_page_page
+    
+    # Then I should bypass authentication.
+    page.current_path.should == new_page_page
+  end
 end
