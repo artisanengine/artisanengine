d0176451d3a4497acbce1842b575bae7c5297d09
Reade Harris <services@artisanengine.com>
2011-07-23 20:00:47 -0400

    page layout and sidebar, social networking links

--- app/controllers/visit/pages_controller.rb
+++ app/controllers/visit/pages_controller.rb
@@ -1,6 +1,7 @@
 module Visit
   class PagesController < Visit::VisitController
     respond_to :html
+    layout     :pages_or_visit
     
     expose( :pages ) { current_frame.pages }
     expose( :page )
@@ -12,5 +13,16 @@ module Visit
     def home
     end
   
+    # ------------------------------------------------------------------
+    private
+
+    def pages_or_visit
+      # Use the normal layout for the home page.
+      return 'visit' if request.path == root_path
+      
+      # Use the page-specific layout if it exists.
+      template_exists?( "layouts/pages" ) ? 'pages' : 'visit'
+    end
+    
   end
 end
