3fc8343b493ebeb80e9437b82582bd41a5713ca7
Reade Harris <services@artisanengine.com>
2011-08-17 16:53:12 -0400

    edit and delete promotions

--- app/controllers/manage/promotions_controller.rb
+++ app/controllers/manage/promotions_controller.rb
@@ -9,5 +9,15 @@ module Manage
       flash[ :notice ] = "Promotion: #{ promotion.promotional_code } was successfully created." if promotion.save
       respond_with :manage, promotion, location: manage_promotions_path
     end
+    
+    def update
+      flash[ :notice ] = "Promotion: #{ promotion.promotional_code } was successfully updated." if promotion.update_attributes( params[ :promotion ] )
+      respond_with :manage, promotion, location: manage_promotions_path
+    end
+    
+    def destroy
+      flash[ :notice ] = "Promotion: #{ promotion.promotional_code } was successfully destroyed." if promotion.destroy
+      respond_with :manage, promotion
+    end
   end
 end
