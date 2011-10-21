a3ba73ed72c2c1f25029ca19db9344ae5ac8b88b
Reade Harris <services@artisanengine.com>
2011-10-21 15:56:27 -0700

    Country name convenience method

--- app/models/address.rb
+++ app/models/address.rb
@@ -12,4 +12,8 @@ class Address < ActiveRecord::Base
   
   validates_presence_of :first_name, :last_name, :address_1, :city, 
                         :country, :postal_code, :frame
+  
+  def country_name
+    Carmen.country_name( country )
+  end
 end
